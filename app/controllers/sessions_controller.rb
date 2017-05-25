918ab4d33d94c8e01ea7b1e0125d8660dbec4778
janeosaur <janewie@gmail.com>
2017-05-24 22:42:26 -0700

    user auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,27 @@
 class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user_params = params.require(:user).permit(:email, :password)
+    @user = User.confirm(user_params)
+    if @user
+      login(@user)
+      flash[:notice] = "You're logged in!"
+      redirect_to  @user
+    else
+      flash[:error] = "Wrong email/password combo!"
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    logout
+    flash[:notice] = "You've logged out!"
+    redirect_to root_path
+  end
+
+
 end
