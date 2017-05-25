572a07c60c3b7c43a378f7701ae2f90f5915604b
janeosaur <janewie@gmail.com>
2017-05-25 11:36:04 -0700

    library show

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -4,6 +4,10 @@ class LibrariesController < ApplicationController
     @libraries = Library.all
   end
 
+  def show
+    @library = Library.find(params[:id])
+  end
+
   def new
     @library = Library.new
   end
